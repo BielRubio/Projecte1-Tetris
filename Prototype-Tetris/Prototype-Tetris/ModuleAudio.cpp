d95bcff0deef1876302a3a78f179007ad5f2aa09
Maksym203 <maksym.polupan@estudiantat.upc.edu>
2022-04-25 10:20:53 +0200

    Globals.cpp fix

--- Prototype-Tetris/Prototype-Tetris/ModuleAudio.cpp
+++ Prototype-Tetris/Prototype-Tetris/ModuleAudio.cpp
@@ -4,7 +4,7 @@
 
 #include "External_Libraries/SDL/include/SDL.h"
 #include "External_Libraries/SDL_mixer/include/SDL_mixer.h"
-#pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
+#pragma comment( lib, "External_Libraries/SDL_mixer/libx86/SDL2_mixer.lib" )
 
 ModuleAudio::ModuleAudio(bool startEnabled) : Module(startEnabled)
 {
